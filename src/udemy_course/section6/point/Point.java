c17d881d25afcc1647d8ecc030ad238754434895
Sami Serbey <samiserbey@MacBook-Pro-de-Sami.local>
2022-09-21 23:04:26 +0200

    constructor point exercise

---
+++ src/udemy_course/section6/point/Point.java
@@ -0,0 +1,35 @@
+package udemy_course.section6.point;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+
+    }
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public double distance() {
+        return Math.sqrt(this.x*this.x + this.y*this.y);
+    }
+    public double distance(int x, int y) {
+        return Math.sqrt((this.x-x)*(this.x-x) + (this.y-y)*(this.y-y));
+    }
+    public double distance(Point p) {
+        return distance(p.getX(), p.getY());
+    }
+}
