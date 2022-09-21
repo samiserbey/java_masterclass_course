b9984a281c84a7c6ee973035bc2344dfacb0db81
Sami Serbey <samiserbey@MacBook-Pro-de-Sami.local>
2022-09-21 21:37:20 +0200

    start section 6

---
+++ src/udemy_course/section6/README.md
@@ -0,0 +1,6 @@
+# Section 6: OOP Part 1 - Classes, Constructors and Inheritance
+- classes are regular data type on steroids
+
+## Classes
+- instance variables/fields should be private `private double firstNumber;`
+- getters, setters
---
+++ src/udemy_course/section6/car/Main.java
@@ -0,0 +1,11 @@
+package udemy_course.section6.car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+        System.out.println("Model is " + porsche.getModel());
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
+    }
+}
